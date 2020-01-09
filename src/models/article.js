4abcce4f78e2cd3653db173894b2becb2adc5252
Gherghe Geani <gherghe_geani@yahoo.com>
2020-01-09 23:56:17 +0200

    Add author && date in article schema.

--- src/models/article.js
+++ src/models/article.js
@@ -5,6 +5,12 @@ const articleSchema = mongoose.Schema({
     type: String,
     required: [true, "You don't get the point. You must say something. Like, 'I love potatoes', maybe?"],
   },
+  author: {
+    type: String,
+  },
+  date: {
+    type: String,
+  },
 });
 
 const articleModel = mongoose.model('article', articleSchema);
